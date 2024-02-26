a4d360343d76be6a9e90be039fc8432ad44e4534
De Andre King <dkinguag@gmail.com>
2024-02-25 21:35:11 -0500

    add: few more tweaks & edits pt.3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ btn.addEventListener("click", ()=>{
         presentBox.style.backgroundColor = "#FFF"
         presentBox.style.color = "black"
         presentBox.style.boxShadow = ".1px .1px 14px 0.1px #a19f9f"
-        btn.innerHTML = "Dark"
+        btn.innerHTML = "Dark Mode (Beta)"
     } 
 })
 
